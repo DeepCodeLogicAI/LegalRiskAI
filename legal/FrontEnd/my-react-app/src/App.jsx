748a01c36ebf8b06205261986ac5e0b1ddcc9b02
eunhye <dmsgp2627@naver.com>
2026-01-28 11:18:09 +0900

    fix: sidebar app 추가 등록

--- legal/FrontEnd/my-react-app/src/App.jsx
+++ legal/FrontEnd/my-react-app/src/App.jsx
@@ -5,25 +5,32 @@ import Jogi from "./pages/jogi.jsx";
 import Header from "./component/Header.jsx";
 import Footer from "./component/Footer.jsx";
 import Boonjang from "./pages/boonjang.jsx";
+import Sidebar from "./component/Sidebar.jsx";
 
 export default function App() {
   return (
     // 전체 레이아웃: 세로 플렉스 + 최소 높이 100vh
     <div className="min-h-screen flex flex-col bg-gray-50">
-      {/* 공통 헤더 */}
+      {/* ✅ 헤더: 회원정보만 */}
       <Header />
 
-      {/* 메인 콘텐츠 영역 */}
-      <main className="flex-1">
-        <Routes>
-          <Route path="/law" element={<Law />} />
-          <Route path="/boonjang" element={<Boonjang />} />
-          <Route path="/yusa" element={<Yusa />} />
-          <Route path="/jogi" element={<Jogi />} />
-        </Routes>
-      </main>
 
-      {/* 공통 푸터 (항상 아래) */}
+      <div className="flex flex-1">
+        {/* 좌측 사이드바 */}
+        <Sidebar />
+
+        {/* 우측 메인 콘텐츠 */}
+        <main className="flex-1 p-6">
+          <Routes>
+            <Route path="/law" element={<Law />} />
+            <Route path="/boonjang" element={<Boonjang />} />
+            <Route path="/yusa" element={<Yusa />} />
+            <Route path="/jogi" element={<Jogi />} />
+          </Routes>
+        </main>
+      </div>
+
+      {/* ✅ 푸터 */}
       <Footer />
     </div>
   );
